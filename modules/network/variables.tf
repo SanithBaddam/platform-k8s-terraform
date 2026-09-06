638209b43e1aad706e5d57f8b817d5f9888e911d
SanithBaddam <67678979+SanithBaddam@users.noreply.github.com>
2026-09-06 00:41:06 -0400

    feat: add modules/network/variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,6 @@
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "name" { type = string }
+variable "address_space" { type = list(string) }
+variable "aks_subnet_prefixes" { type = list(string) }
+variable "tags" { type = map(string) default = {} }
